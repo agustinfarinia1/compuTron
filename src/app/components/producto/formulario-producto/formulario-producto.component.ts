28397de51adf9dd5d0e9a640de2caa72c5b423d4
agustinfarinia1 <agustin.farinia@gmail.com>
2024-10-24 19:55:39 -0300

    finalizacion carga e implementacion de edicion de productos

--- src/app/components/producto/formulario-producto/formulario-producto.component.ts
+++ src/app/components/producto/formulario-producto/formulario-producto.component.ts
@@ -1,49 +1,70 @@
-import { Component, Input, model, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Producto } from '../../../models/producto.model';
-import { FormsModule } from '@angular/forms';
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiMlService } from '../../../services/api-ml.service';
 import { CommonModule } from '@angular/common';
 import { CategoriasJsonServerService } from '../../../services/categorias-json-server.service';
+import { ProductosJsonServerService } from '../../../services/productos-json-server.service';
 
 @Component({
   selector: 'app-formulario-producto',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './formulario-producto.component.html',
   styleUrl: './formulario-producto.component.css'
 })
 export class FormularioProductoComponent{
+  //@Input() idProducto: string; // Se debera implementar una obtencion del id por el url
 
-  @Input() accionFormulario!: (producto:Producto) => void;
+  productoFormulario : FormGroup;
+  respuestaBusqueda : [] = [];
+  respuestaCategorias = [];
+  idProducto : string = "P1";
+  tituloFormulario = "Carga Productos";
+  textoBoton = "Cargar";
+  cantidadProductos : number;
 
-  constructor(private servicioMl:ApiMlService,private categoriasServicio: CategoriasJsonServerService){}
+  constructor(private servicioMl:ApiMlService,private categoriasServicio: CategoriasJsonServerService,private productosServicio: ProductosJsonServerService){
+    this.cantidadProductos = 0;
+    this.productoFormulario = new FormGroup({
+      busqueda : new FormControl(),
+      codigoML : new FormControl("",[Validators.required,Validators.minLength(3)],),
+      titulo : new FormControl("",[Validators.required,Validators.minLength(3)]),
+      categoria : new FormControl(null,[Validators.required]),
+      marca : new FormControl("",[Validators.required,Validators.minLength(3)]),
+      modelo : new FormControl("",[Validators.required,Validators.minLength(3)]),
+      cantidad : new FormControl(1,[Validators.required,Validators.min(1)]),
+      precio : new FormControl(1000,[Validators.required,Validators.min(1000)]),
+      imagen : new FormControl("",[Validators.required,Validators.minLength(5)]),
+    })
+  }
 
   ngOnInit(): void {
     this.categoriasServicio.getCategorias().then((respuesta) => this.respuestaCategorias = respuesta);
+    this.productosServicio.getCantidadProductos().then((respuestaCantidad) => this.cantidadProductos = respuestaCantidad);
+    if(this.idProducto){
+      this.productosServicio.consultarCodigo(this.idProducto).then((respuestaConsultaId) => {
+        let respuestaProducto = respuestaConsultaId[0];
+        if(respuestaProducto){ // Si existe el producto que tiene que buscar por ID, Cambia el modo a Edicion
+          this.tituloFormulario = "Editar Producto";
+          this.textoBoton = "Editar";
+          if(this.respuestaCategorias){
+            let numeroCategoria = this.respuestaCategorias.findIndex((respuestaASADSD:any) => respuestaConsultaId[0].categoria === respuestaASADSD.id);
+            this.productoFormulario.get("categoria")?.setValue(numeroCategoria);
+          }
+          this.productoFormulario.get("codigoML")?.patchValue(respuestaProducto.codigoML);
+          this.productoFormulario.get("titulo")?.patchValue(respuestaProducto.titulo);
+          this.productoFormulario.get("marca")?.patchValue(respuestaProducto.marca);
+          this.productoFormulario.get("modelo")?.patchValue(respuestaProducto.modelo);
+          this.productoFormulario.get("precio")?.patchValue(respuestaProducto.precio);
+          this.productoFormulario.get("imagen")?.patchValue(respuestaProducto.imagen);
+        }
+      });
+    }
   }
-
-  campoBusqueda : string = "";
-  campoCodigo : string = "";
-  campoTitulo : string = "";
-  campoCategoria : number = 0;
-  campoMarca : string = "";
-  campoModelo : string = "";
-  campoCantidad : number = 1;
-  campoPrecio : number = 1;
-  campoImagen : string = "";
-  respuestaBusqueda : [] = [];
-  respuestaCategorias = [];
 
   buscarProductos = () => {
-    this.servicioMl.getData(this.campoBusqueda).then((e) => this.respuestaBusqueda = e);
-  }
-
-  seleccionCategoria = (valor : Event) => {
-    const selectElement = valor.target as HTMLSelectElement;
-    const categoria : any = this.respuestaCategorias[parseInt(selectElement.value)];
-    if(categoria){
-      this.campoCategoria = categoria.valor;
-    }
+    this.servicioMl.getData(this.productoFormulario.get("busqueda")?.value).then((e) => this.respuestaBusqueda = e);
   }
 
   seleccionProducto = (valor : Event) => {
@@ -52,24 +73,50 @@ export class FormularioProductoComponent{
     const marca = this.obtenerCampo(producto["attributes"],"BRAND");
     const modelo = this.obtenerCampo(producto["attributes"],"MODEL");
     if(producto){
-      this.campoCodigo = producto.id;
-      this.campoTitulo = producto.title;
-      this.campoPrecio = producto.price;
-      this.campoImagen = producto.thumbnail;
+      this.productoFormulario.get("codigoML")?.setValue(producto.id);
+      this.productoFormulario.get("titulo")?.setValue(producto.title);
+      this.productoFormulario.get("precio")?.setValue(producto.price);
+      this.productoFormulario.get("imagen")?.setValue(producto.thumbnail);
       if(marca){
-        this.campoMarca = marca;
+        this.productoFormulario.get("marca")?.setValue(marca);
       }
       if(modelo){
-        this.campoModelo = modelo;
+        this.productoFormulario.get("modelo")?.setValue(modelo);
       }
     }
   }
-obtenerCampo = (producto : any[],campo : string) => {
-    return producto.find((f) => f.id === campo).value_name;
-}
+
+  obtenerCampo = (producto : any[],campo : string) => {
+      return producto.find((f) => f.id === campo).value_name;
+  }
+
+  seleccionCategoria = (valor : Event) => {
+    const selectElement = valor.target as HTMLSelectElement;
+    this.productoFormulario.get("categoria")?.setValue(selectElement.value);
+  }
+
+  onSubmit = () => {
+    if(this.productoFormulario.valid){
+      this.submitFormulario();
+    }
+    else{
+      this.productoFormulario.markAllAsTouched();
+    }
+  }
 
   submitFormulario = () =>{
-    let producto : Producto = new Producto(this.campoCodigo,this.campoTitulo,this.campoCategoria,this.campoMarca,this.campoModelo,this.campoCantidad,this.campoPrecio,this.campoImagen);
-    this.accionFormulario(producto);
+    let producto : Producto = new Producto(this.productoFormulario.get("codigoML")?.value,this.productoFormulario.get("titulo")?.value,this.productoFormulario.get("categoria")?.value,this.productoFormulario.get("marca")?.value,this.productoFormulario.get("modelo")?.value,this.productoFormulario.get("cantidad")?.value,this.productoFormulario.get("precio")?.value,this.productoFormulario.get("imagen")?.value);
+    if(this.idProducto){
+      let indexCategoria =  this.productoFormulario.get("categoria")?.value;
+      if(indexCategoria){
+        producto.setId(this.idProducto);
+        producto.setCategoria(this.respuestaCategorias[indexCategoria]["id"]);
+        this.productosServicio.editarProducto(producto);
+      }
+    }
+    else{
+      producto.setId(`P${this.cantidadProductos}`);
+      this.productosServicio.setNewProducto(producto);
+    }
   }
 }
--- src/app/models/producto.model.ts
+++ src/app/models/producto.model.ts
@@ -1,8 +1,11 @@
+import { ProductosJsonServerService } from "../services/productos-json-server.service";
+
 export class Producto{
 
-  private codigo : string;
+  private id : string;
+  private codigoML : string;
   private titulo : string;
-  private categoria : number;
+  private categoria : string;
   private marca : string;
   private modelo : string;
   private cantidad : number;
@@ -10,8 +13,9 @@ export class Producto{
   private imagen : string;
   private eliminado : boolean;
 
-  constructor(newCodigo:string,newTitulo:string,newCategoria:number,newMarca:string,newModelo : string,newCantidad:number,newPrecio : number,newImagen : string){
-    this.codigo = newCodigo;
+  constructor(newCodigoML : string,newTitulo : string,newCategoria : string,newMarca : string,newModelo : string,newCantidad : number,newPrecio : number,newImagen : string,newId : string = ""){
+    this.id = newId;
+    this.codigoML = newCodigoML;
     this.titulo = newTitulo;
     this.categoria = newCategoria;
     this.marca = newMarca;
@@ -22,11 +26,19 @@ export class Producto{
     this.eliminado = false;
   }
 
+  setId = (newId : string) => {
+    this.id = newId;
+  }
+
+  setCodigoML = (newCodigoML : string) => {
+    this.codigoML = newCodigoML;
+  }
+
   setTitulo = (newTitulo : string) => {
     this.titulo = newTitulo;
   }
 
-  setCategoria = (newCategoria : number) => {
+  setCategoria = (newCategoria : string) => {
     this.categoria = newCategoria;
   }
 
@@ -54,8 +66,12 @@ export class Producto{
     this.eliminado = newEliminado;
   }
 
-  getCodigo = ()  => {
-    return this.codigo;
+  getId = () => {
+    return this.id;
+  }
+
+  getCodigoML = ()  => {
+    return this.codigoML;
   }
 
   getTitulo = ()  => {
