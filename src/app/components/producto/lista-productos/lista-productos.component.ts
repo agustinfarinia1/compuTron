821a2d5a5dce002c5f17b48d397fd204fc84de61
Ciro Lopez <theirsha17@gmail.com>
2024-10-28 19:33:25 -0300

    kan-12 navegar por el catalogo
    paginacion y ordenar por precios

--- src/app/components/producto/lista-productos/lista-productos.component.ts
+++ src/app/components/producto/lista-productos/lista-productos.component.ts
@@ -4,17 +4,23 @@ import { CommonModule } from '@angular/common';
 import { ProductoItemComponent } from "../producto-item/producto-item.component";
 import { CategoriasJsonServerService } from '../../../services/categorias-json-server.service';
 import { ProductosJsonServerService } from '../../../services/productos-json-server.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-productos',
   standalone: true,
-  imports: [CommonModule, ProductoItemComponent],
+  imports: [CommonModule, ProductoItemComponent, NgxPaginationModule, FormsModule],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
 export class ListaProductosComponent {
 
   listaCategorias : [];
+
+  p:number =1 ;
+
+  ordenSeleccionado = 'default';
 
   @Input() listaProductos:Producto[];
 
@@ -27,4 +33,15 @@ export class ListaProductosComponent {
     this.categoriasServicio.getCategorias().then((respuestaCategorias) => this.listaCategorias = respuestaCategorias);
     this.productosServicio.getProductos().then((respuestaProductos) => this.listaProductos = respuestaProductos);
   }
+
+  ordenarProductos() {
+    if (this.ordenSeleccionado === 'asc') {
+      this.listaProductos.sort((a, b) => a.getPrecio() - b.getPrecio());
+    } else if (this.ordenSeleccionado === 'desc'){
+      this.listaProductos.sort((a, b) => b.getPrecio() - a.getPrecio());
+    }else{
+      this.productosServicio.getProductos().then((respuestaProductos) => this.listaProductos = respuestaProductos);
+    }
+  }
+
 }
